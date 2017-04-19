41a39aac1f14e7500766f47eda17e5ab32638284
yuto tomabechi <gurepon213@yahoo.co.jp>
2017-04-19 06:50:42 +0000

    kadai-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -48,6 +48,7 @@ class TasksController < ApplicationController
   end
   
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
+     
   end
 end
