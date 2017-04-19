f5acf1c8dc87b9b4992f653c9909c2a9c1d90f19
yuto tomabechi <gurepon213@yahoo.co.jp>
2017-04-19 05:28:28 +0000

    kadai-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,6 +12,15 @@ class TasksController < ApplicationController
   end
 
   def create
+       @task = Task.new(task_params)
+
+    if @task.save
+      flash[:success] = 'Task が正常に投稿されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task が投稿されませんでした'
+      render :new
+    end
   end
 
   def edit
@@ -19,8 +28,26 @@ class TasksController < ApplicationController
   end
 
   def update
+       @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      flash[:success] = 'Task は正常に更新されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task は更新されませんでした'
+      render :edit
+    end
   end
 
   def destroy
+      @task = Task.find(params[:id])
+    @task.destroy
+
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_to tasks_url
+  end
+  
+  def task_params
+    params.require(:task).permit(:content)
   end
 end
