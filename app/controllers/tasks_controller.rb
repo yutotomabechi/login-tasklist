c7aee59fe5cb8f1b390406d024d9ef58727ab45a
yuto tomabechi <gurepon213@yahoo.co.jp>
2017-04-20 04:07:31 +0000

    bootstrap

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,15 @@
 class TasksController < ApplicationController
+   before_action :set_task, only: [:show, :edit, :update, :destroy]
      def index
          @tasks = Task.all.page(params[:page]).per(3)
          end
 
   def show
-      @task = Task.find(params[:id])
+      
   end
 
   def new
-    @task = Task.new  
+    @task = Task.new
   end
 
   def create
@@ -24,11 +25,11 @@ class TasksController < ApplicationController
   end
 
   def edit
-      @task = Task.find(params[:id])
+      
   end
 
   def update
-       @task = Task.find(params[:id])
+       
 
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
@@ -40,11 +41,17 @@ class TasksController < ApplicationController
   end
 
   def destroy
-      @task = Task.find(params[:id])
+      
     @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
     redirect_to tasks_url
+  end
+   
+   private
+
+  def set_task
+    @task = Task.find(params[:id])
   end
   
   def task_params
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-Task.create(content: 'test content 1', status: 'test status 1')
-Task.create(content: 'test content 2', status: 'test status 2')
-Task.create(content: 'test content 3', status: 'test status 3')
+(1..50).each do |number|
+  Task.create!(content: 'test content ' + number.to_s, status: 'st' + number.to_s)
+end
