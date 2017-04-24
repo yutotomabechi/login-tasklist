62a3b96eddcd4e985cc908367d0680cd9b53bcb4
yuto tomabechi <gurepon213@yahoo.co.jp>
2017-04-24 05:53:59 +0000

    twitter-kadai1

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -2,7 +2,7 @@ class ToppagesController < ApplicationController
  def index
     if logged_in?
       @user = current_user
-      @task = current_user.tasks.build  # form_for 用
+      @task = current_user.tasks.build  
       @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
     end
   end
