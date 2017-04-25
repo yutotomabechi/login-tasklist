b1a069b9de2c56d88336df23682ba7421eb2446d
yuto tomabechi <gurepon213@yahoo.co.jp>
2017-04-25 00:51:30 +0000

    twitter-kadai1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     password = params[:session][:password]
     if login(email, password)
       flash[:success] = 'ログインに成功しました。'
-      redirect_to 'toppages#index'
+      redirect_to @user
     else
       flash.now[:danger] = 'ログインに失敗しました。'
       render 'new'
